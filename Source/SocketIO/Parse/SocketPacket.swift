814e9655a74576bfab270c9561903e02369f536b
Erik Little <nuclear.ace@gmail.com>
2017-10-18 09:04:37 -0400

    Another reduce(into:)

--- Source/SocketIO/Parse/SocketPacket.swift
+++ Source/SocketIO/Parse/SocketPacket.swift
@@ -142,12 +142,8 @@ public struct SocketPacket : CustomStringConvertible {
             if dict["_placeholder"] as? Bool ?? false {
                 return binary[dict["num"] as! Int]
             } else {
-                return dict.reduce(JSON(), {cur, keyValue in
-                    var cur = cur
-
+                return dict.reduce(into: JSON(), {cur, keyValue in
                     cur[keyValue.0] = _fillInPlaceholders(keyValue.1)
-
-                    return cur
                 })
             }
         case let arr as [Any]:
